3ddbf60791342bc247c36a178de9d6b8dc0b2876
said moutaa <smoutaa@gmail.com>
2019-12-16 23:12:44 +0100

    setup all the environment and adding routes

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <div> hello from the home page</div>
+        </div>
+    )
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Rooms = () => {
+    return (
+        <div>
+            hello from the rooms page 
+        </div>
+    )
+}
+
+export default Rooms;
