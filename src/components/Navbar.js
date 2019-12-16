40574f7e069267cb98902a10414021d76c296df1
said moutaa <smoutaa@gmail.com>
2019-12-16 23:41:55 +0100

    Stoping at 41:48

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import logo from '../images/logo.svg'
+import {FaAlignRight} from 'react-icons/fa'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                hello from navbar
+            </div>
+        )
+    }
+}
